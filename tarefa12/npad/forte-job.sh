529bfbb2b2ff49b96923b5e45ecd9b1a8ca5deaa
polianaraujo <polianaellena123@gmail.com>
2025-05-17 13:38:04 -0300

    npad

--- tarefa12/npad/forte-job.sh
+++ tarefa12/npad/forte-job.sh
@@ -12,7 +12,7 @@ gcc -fopenmp ns_esc_forte.c -o escalabilidade_forte
 # Execuções variando threads
 # for threads in 1 2 4 8 16 24
 # do
-export OMP_NUM_THREADS=$threads
+# export OMP_NUM_THREADS=$threads
 echo "Executando com $OMP_NUM_THREADS threads"
 /home/pedarajo/repos/parallelp/tarefa12/npad/escalabilidade_forte
 # done
