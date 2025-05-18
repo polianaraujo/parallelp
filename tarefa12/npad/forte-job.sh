558f8a68a95484c593d59975a902b82066b44d6b
polianaraujo <polianaellena123@gmail.com>
2025-05-17 23:04:29 -0300

    npad

--- tarefa12/npad/forte-job.sh
+++ tarefa12/npad/forte-job.sh
@@ -17,4 +17,17 @@ echo "Executando escalabilidade forte com variação de threads e tamanho do pro
 /home/pedarajo/repos/parallelp/tarefa12/npad/escalabilidade_forte
 # done
 
-echo "Finalizando JOB: Escalabilidade Forte"
+
+echo "Inicializando JOB: Escalabilidade Fraca"
+
+# Compilação
+gcc -fopenmp ns_esc_fraca.c -o escalabilidade_fraca
+
+# Execuções variando threads
+# for threads in 1 2 4 8 16 24
+# do
+# export OMP_NUM_THREADS=$threads
+echo "Executando escalabilidade fraca com variação de threads e tamanho do problema"
+/home/pedarajo/repos/parallelp/tarefa12/npad/escalabilidade_fraca
+
+echo "Finalizando JOB: Escalabilidade Fraca"
