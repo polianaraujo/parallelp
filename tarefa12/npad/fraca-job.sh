808bff348cae6afe2b5a2317a9e80f8f11374d12
polianaraujo <polianaellena123@gmail.com>
2025-05-17 22:59:46 -0300

    npad

--- tarefa12/npad/fraca-job.sh
+++ tarefa12/npad/fraca-job.sh
@@ -7,22 +7,14 @@
 echo "Inicializando JOB: Escalabilidade Fraca"
 
 # Compilação
-echo "Compilando ns_esc_fraca.c..."
 gcc -fopenmp ns_esc_fraca.c -o escalabilidade_fraca
-echo "Compilação concluída."
 
-# Listar conteúdo do diretório atual
-echo "Arquivos no diretório:"
-ls -lh
-
-# Verificar se o executável existe
-if [ ! -f escalabilidade_fraca ]; then
-    echo "Erro: executável não encontrado."
-    exit 1
-fi
-
-# Executar o programa
-echo "Executando ./escalabilidade_fraca"
+# Execuções variando threads
+# for threads in 1 2 4 8 16 24
+# do
+# export OMP_NUM_THREADS=$threads
+echo "Executando escalabilidade fraca com variação de threads e tamanho do problema"
 /home/pedarajo/repos/parallelp/tarefa12/npad/escalabilidade_fraca
+# done
 
 echo "Finalizando JOB: Escalabilidade Fraca"
