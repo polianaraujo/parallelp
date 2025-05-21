4766a47c81d44ac6d99e238323f7328356d83d45
Poliana Ellen de Araújo <pedarajo@service0.npad.internal>
2025-05-21 16:24:00 -0300

    test

--- tarefa18/job_submit.sh
+++ tarefa18/job_submit.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=heat-gpu-poliana
+#SBATCH --job-name=heat
 #SBATCH --output=slurm-heat.out
 #SBATCH --error=slurm-heat.err
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --gpus=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu-8-v100
 #SBATCH --time=00:10:00
-#SBATCH --partition=gpu-8-v100
 
-# Carrega os módulos necessários
-# module load gcc/12.2.0
-# module load openmpi/4.1.4
-# module load cuda/12.2.0
+// module load nvhpc
 
-# Compilação com suporte a OpenMP offloading para GPU NVIDIA
-gcc -fopenmp -foffload=nvptx-none -O3 heat.c -o heat
+nvc -mp=gpu -O3 heat.c -o heat
 
-# Executa o programa com grade 1000x1000 e 100 passos de tempo
-./heat 1000 100
+./heat 1000 10
+
