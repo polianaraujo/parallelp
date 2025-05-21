be200d42a45244a7965ccfb1f0b83f817faea3a5
polianaraujo <polianaellena123@gmail.com>
2025-05-21 09:49:34 -0300

    code

--- tarefa16/job_submit.sh
+++ tarefa16/job_submit.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=slurm-matvec-%j.out
 #SBATCH --time=0-0:30
 #SBATCH --partition=intel-128
-#SBATCH --ntasks=16
+#SBATCH --ntasks=8
 #SBATCH --cpus-per-task=1
 
 # module load mpi/openmpi-x86_64
@@ -15,7 +15,7 @@ echo "M,N,Num_processos,Tempo_segundos" > resultados.csv
 
 for M in 1000 10000 50000; do
   for N in 100 1000; do
-    for NP in 2 4 8 16; do
+    for NP in 2 4 8; do
       if (( M % NP == 0 )); then
         echo "=== Executando M=$M N=$N com $NP processos ==="
         mpirun --mca btl ^openib -np $NP ./matrix_vector_mpi $M $N
