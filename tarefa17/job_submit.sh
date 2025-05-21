e626394bf704aec739667266a32a4e801d6525e6
polianaraujo <polianaellena123@gmail.com>
2025-05-21 12:08:14 -0300

    code

--- tarefa17/job_submit.sh
+++ tarefa17/job_submit.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
-#SBATCH --job-name=matvec-col
-#SBATCH --output=slurm-matvec-col-%j.out
-#SBATCH --time=0-0:20
+#SBATCH --job-name=matrix-vector-mpi
+#SBATCH --output=slurm-matvec-%j.out
+#SBATCH --time=0-0:30
 #SBATCH --partition=intel-256
-#SBATCH --ntasks=4
+#SBATCH --ntasks=8
 #SBATCH --cpus-per-task=1
 
 # module load mpi/openmpi-x86_64
 
-mpicc -o matvec_col matvec_col.c
-mpirun -np 4 ./matvec_col
+mpicc matrix_vector_mpi.c -o matrix_vector_mpi -O2
+
+# Cabeçalho do CSV
+echo "M,N,Num_processos,Tempo_segundos" > resultados.csv
+
+for M in 1000 10000 50000; do
+  for N in 100 1000; do
+    for NP in 2 4 8; do
+      if (( N % NP == 0 )); then
+        echo "=== Executando M=$M N=$N com $NP processos ==="
+        mpirun --mca btl ^openib -np $NP ./matrix_vector_mpi $M $N
+        echo ""
+      else
+        echo ">>> Pulando N=$N com $NP processos (não divisível)"
+      fi
+    done
+  done
+done
