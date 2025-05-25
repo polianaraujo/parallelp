3db2115375766c83d64a63e4e5e23d628ced7721
polianaraujo <polianaellena123@gmail.com>
2025-05-24 23:02:11 -0300

    tarefa18

--- tarefa18/job_submit.sh
+++ tarefa18/job_submit.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-#SBATCH --job-name=heat_poliana
+#SBATCH --job-name=heat_poliana_cpu
 #SBATCH --output=slurm-heat.out
 #SBATCH --error=slurm-heat.err
-#SBATCH --partition=gpu-8-v100 
-#SBATCH --gpus-per-node=1
+#SBATCH --partition=cpu-small
 #SBATCH --time=0-00:10
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
 
 set -x
 
-echo "Iniciando job SLURM..."
+echo "Iniciando job SLURM (CPU)..."
 
 module purge
 module load gcc/12.2.0
-module load cuda/11.8
 
 echo "Compilando o código..."
-gcc -fopenmp -foffload=nvptx-none -O3 -o heat heat.c -lm
+gcc -fopenmp -O3 -o heat heat.c -lm
 
 echo "Listando arquivos após compilação:"
 ls -lh
