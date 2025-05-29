78479d49a1392602dbfb24fe0b1598351c774f33
polianaraujo <polianaellena123@gmail.com>
2025-05-28 23:30:40 -0300

    delet

--- tarefa18/teste_gpu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=test_simple
-#SBATCH --output=teste_simple.out
-#SBATCH --error=teste_simple.err
-#SBATCH --time=0-03:00
-
-echo "Testando SLURM no NPAD - GPU partition"
-nvidia-smi
-
